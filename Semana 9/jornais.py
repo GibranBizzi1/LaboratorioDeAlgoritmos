94bdca181e4e556e18055815a687010347468ca0
GibranBizzi1 <gibranbizzi1@gmail.com>
2025-05-05 21:46:18 -0300

    jornais.py

--- Semana 9/jornais.py
+++ Semana 9/jornais.py
@@ -1,18 +1,46 @@
-a = 0
+a = 0 
 b = 0
 c = 0
 
-for n in range (1,21):
-    jornal = input("Digite qual vc mais lê, A, B ou C").upper()
+for n in range(1, 21):
+    jornal = input("Digite qual você mais lê, A, B ou C: ").upper()
+    
     if jornal == "A":
-        a += 1
+        a = a + 1
     elif jornal == "B":
-        b += 1
+        b = b + 1
     elif jornal == "C":
-        c += 1
+        c = c + 1
     else:
-        print ("inválido")
+        print("Invalido")
 
-print ("A porcentagem que lê o jornal a é de:", (a / 20)* 100)
-print ("A porcentagem que lê o jornal b é de:", (b / 20)* 100)
-print ("A porcentagem que lê o jornal c é de:", (c / 20)* 100)
+porc_a = (a * 100) / 20
+porc_b = (b * 100) / 20
+porc_c = (c * 100) / 20
+
+if porc_a <= porc_b and porc_a <= porc_c:
+    print("A:", porc_a)
+    if porc_b <= porc_c:
+        print("B:", porc_b)
+        print("C:", porc_c)
+    else:
+        print("C:", porc_c)
+        print("B:", porc_b)
+
+elif porc_b <= porc_a and porc_b <= porc_c:
+    print("B:", porc_b)
+    if porc_a <= porc_c:
+        print("A:", porc_a)
+        print("C:", porc_c)
+    else:
+        print("C:", porc_c)
+        print("A:", porc_a)
+
+else:
+    print("C:", porc_c)
+    if porc_a <= porc_b:
+        print("A:", porc_a)
+        print("B:", porc_b)
+    else:
+        print("B:", porc_b)
+        print("A:", porc_a)
